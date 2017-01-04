a9c8459a248a004e044fb1ed37157f92a85ff0ff
Blase <blasemaglaya@gmail.com>
2017-01-03 20:34:29 -0800

    Quick start

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 var express = require('express');
-
 var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.get('/', function (req, res) {
+	res.send('Hello world!');
+});
+
+app.listen(port, function () {
+	console.log('App now running on port: ', port );
+});
