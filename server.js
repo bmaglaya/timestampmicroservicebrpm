e4d5a87f323d1d72b0636bc83f4772d41cbcc72b
Blase <blasemaglaya@gmail.com>
2017-01-03 21:31:03 -0800

    Serve index page

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require('express');
+var path = require('path');
+
 var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-	res.send('Hello world!');
-});
-
 app.listen(port, function () {
 	console.log('App now running on port: ', port );
+});
+
+/*
+ *	Timestamp Microservice API Routes
+ *
+ *	GET ('/') displays index page.
+ *	GET ('/:timestamp') takes time stamp and returns
+ * 		{ "unix": unixTimeStamp, "natural": "Month Day, Year" }
+ *		or returns null for both with an invalid time stamp.
+ *
+ */
+ 
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
